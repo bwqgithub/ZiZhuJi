fb8ac903b4ef0393ff4ad42365653f61628f6ea4
毕文强 <bi-wenqiang@outlook.com>
2020-05-15 10:04:56 +0800

    建立项目

---
+++ CefCustomObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Diagnostics;
+
+namespace ZiZhuJi
+{
+    class CefCustomObject
+    {
+        // Declare a local instance of chromium and the main form in order to execute things from here in the main thread
+        private static ChromiumWebBrowser _instanceBrowser = null;
+        // The form class needs to be changed according to yours
+        private static FormMain _instanceMainForm = null;
+
+
+        public CefCustomObject(ChromiumWebBrowser originalBrowser, FormMain mainForm)
+        {
+            _instanceBrowser = originalBrowser;
+            _instanceMainForm = mainForm;
+        }
+
+        public void showDevTools()
+        {
+            _instanceBrowser.ShowDevTools();
+        }
+
+        public void opencmd()
+        {
+            ProcessStartInfo start = new ProcessStartInfo("cmd.exe", "/c pause");
+            Process.Start(start);
+        }
+    }
+}
---
+++ FormMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Runtime.InteropServices;
+
+namespace ZiZhuJi
+{
+    public partial class FormMain : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            InitializeChromium();
+
+            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+            chromeBrowser.JavascriptObjectRepository.Register("cefCustomObject", new CefCustomObject(chromeBrowser, this), isAsync: true, options: BindingOptions.DefaultBinder);
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            String page = string.Format(@"{0}\www\index.html", Application.StartupPath);
+            if (!File.Exists(page))
+            {
+                MessageBox.Show("Error The html file doesn't exists : " + page);
+            }
+            Cef.Initialize(settings);
+            chromeBrowser = new ChromiumWebBrowser(page);
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+
+            // Allow the use of local resources in the browser
+            BrowserSettings browserSettings = new BrowserSettings();
+            browserSettings.FileAccessFromFileUrls = CefState.Enabled;
+            browserSettings.UniversalAccessFromFileUrls = CefState.Enabled;
+            chromeBrowser.BrowserSettings = browserSettings;
+
+        }
+
+        public void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // TODO
+            Cef.Shutdown();
+        }
+    }
+}
